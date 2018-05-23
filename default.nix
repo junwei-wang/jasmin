413ee537a68e021e83421b0e08a08d2a7a38dbd7
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-05-23 08:51:04 +0000

    Use dpdgraph to search for unused lemmas

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   src = ./.;
   buildInputs = [ coqword ]
     ++ (with python3Packages; [ python pyyaml ])
-    ++ (with coqPackages_8_7; [ coq mathcomp ])
+    ++ (with coqPackages_8_7; [ coq mathcomp dpdgraph ])
     ++ (with ocamlPackages; [ ocaml findlib ocamlbuild batteries menhir merlin zarith ])
     ;
 }
